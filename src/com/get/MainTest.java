87d7051abc224c67e92d3fba2c5859ee03cf298f
Super-Jun-Jie <1970303465@qq.com>
2020-12-17 13:42:12 +0800

    测试提交

---
+++ src/com/get/MainTest.java
@@ -0,0 +1,11 @@
+package com.get;
+
+/**
+ * @Author: 胡俊杰
+ * @Date: 2020/12/17 10:38
+ */
+public class MainTest {
+    public static void main(String[] args) {
+        System.out.println("测试");
+    }
+}
